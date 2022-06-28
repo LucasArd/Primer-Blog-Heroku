5d032b090b300cfc7900dd3e92a334a8f65957e0
LucasArd <luqasaranda@outlook.com>
2022-06-28 14:51:40 -0300

    5to

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -12,6 +12,7 @@ from AppAero.views import inicio
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('AppAero/', include('AppAero.urls')),
+    path('', inicio, name='inicio'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
